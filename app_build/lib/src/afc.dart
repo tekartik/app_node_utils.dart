8a62d45f1cf795ced39de83f3a72b2906c8ecb63
alex <alex@tekartik.com>
2020-09-10 12:03:18 +0200

    feat: add afcNodeBuild aliyun support

--- app_build/lib/src/afc.dart
+++ app_build/lib/src/afc.dart
@@ -1,33 +1,28 @@
-// Firebase cloud function
+/// Aliyun function compute
 import 'dart:io';
 
 import 'package:process_run/shell_run.dart';
 import 'package:tekartik_app_node_build/src/run.dart';
 
 /// Compile bin/main.dart to deploy/functions/index.js
-Future gcfNodeBuildAndServe(
-    {String directory = 'bin', String deployDirectory = 'deploy'}) async {
-  await gcfNodeServe(directory: deployDirectory);
-}
-
-Future gcfNodeBuild(
+Future afcNodeBuild(
     {String directory = 'bin', String deployDirectory = 'deploy'}) async {
   await nodeBuild(directory: directory);
-  await gcfNodeCopyToDeploy(
+  await afcNodeCopyToDeploy(
       directory: directory, deployDirectory: deployDirectory);
 }
 
-Future gcfNodeServe({String directory = 'deploy'}) async {
+Future afcNodeServe({String directory = 'deploy'}) async {
   var shell = Shell(workingDirectory: directory);
   await shell.run('firebase serve');
 }
 
 /// Convert main.dart to index.js
-Future gcfNodeCopyToDeploy(
+Future afcNodeCopyToDeploy(
     {String directory = 'bin', String deployDirectory = 'deploy'}) async {
   var src = File('build/$directory/main.dart.js');
   Future copy() async {
-    var file = await src.copy('$deployDirectory/functions/index.js');
+    var file = await src.copy('$deployDirectory/index.js');
     print('copied to ${file} ${file.statSync()}');
   }
 
