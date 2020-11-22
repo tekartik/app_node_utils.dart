29bceed7c38210d653e31e7ba3d8910191c0e5d5
alex <alex@tekartik.com>
2020-11-22 01:38:21 +0100

    feat: use modified shell environment to load the env

--- node_utils/lib/src/node_io.dart
+++ node_utils/lib/src/node_io.dart
@@ -1,3 +1,4 @@
+import 'package:process_run/shell.dart';
 import 'package:tekartik_app_node_utils/src/node_common.dart';
 import 'package:tekartik_platform/context.dart' as pc;
 import 'package:tekartik_platform_io/context_io.dart' as pc;
@@ -5,8 +6,9 @@ import 'package:tekartik_platform_io/context_io.dart' as pc;
 pc.PlatformContext get platformContext => pc.platformContextIo;
 
 class PlatformIo implements Platform {
+  // Use shell environment from process run
   @override
-  Map<String, String> get environment => platformContext.io.environment;
+  Map<String, String> get environment => shellEnvironment;
 }
 
 final platform = PlatformIo();
